3c3a215c61f71d2dff5cedc88bcdee3c51053cc5
Alec Reimel <alecreimel1@gmail.com>
2022-10-03 15:46:05 -0400

    style updates

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 console.log("script.js loaded");
 
-document.addEventListener('DOMContentLoaded', documentReady, false);
+/*document.addEventListener('DOMContentLoaded', documentReady, false);
 window.addEventListener('load', fullPageLoad, false);
 
 function documentReady () {
@@ -11,4 +11,4 @@ function documentReady () {
 function fullPageLoad () {
   // code here...
   console.log("fullPageLoad");
-}
+}*/
