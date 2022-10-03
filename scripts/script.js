f0bce0ad2cdcc2c0be91ad8676aa3807500ef326
Alec Reimel <alecreimel1@gmail.com>
2022-10-02 23:28:06 -0400

    vanilla js document.ready and window.load functions set up

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,14 @@
-console.log("page loaded")
+console.log("script.js loaded");
+
+document.addEventListener('DOMContentLoaded', documentReady, false);
+window.addEventListener('load', fullPageLoad, false);
+
+function documentReady () {
+  // code here...
+  console.log("documentReady");
+}
+
+function fullPageLoad () {
+  // code here...
+  console.log("fullPageLoad");
+}
